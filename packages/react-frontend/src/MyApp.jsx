8d5e0fc8809be38e6263ba612e6787cc7f3d13d1
Brent Nakashima <brentnakashima@Brents-MacBook-Air.local>
2024-04-17 11:45:55 -0700

    IE3 Delete on Backend Done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,20 +1,70 @@
 // src/MyApp.jsx
-// ON STEP 7
-import React, { useState } from "react";
+
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 function MyApp() { 
     const [characters, setCharacters] = useState([
     ]);
+    useEffect(() => {
+      fetchUsers()
+        .then((res) => res.json())
+        .then((json) => setCharacters(json["users_list"]))
+        .catch((error) => { console.log(error); });
+    }, [] );
+    // Edit for delete on backend
     function removeOneCharacter(index) {
+        let id;
+        // This function creates a new array excluding removed
         const updated = characters.filter((character, i) => {
+          if(i === index) {
+            id = character.id;
+          }
           return i !== index;
-        });
+        }); 
+         
         setCharacters(updated);
+        deleteUser(id); // ERROR: HOW DO I GET ID  
+    };
+   
+    function updateList(person) { 
+      postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+          console.log(error);
+        })
     }
-    function updateList(person) {
-        setCharacters([...characters, person]);
-      }
+    function fetchUsers() {
+      const promise = fetch("http://localhost:8000/users");
+      return promise;
+    }
+    function postUser(person) {
+      const promise = fetch("Http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(person),
+      });
+  
+      return promise;
+    }
+    // Purpose: Make the HTTP request deletion
+    function deleteUser(id) {
+      const promise = fetch("Http://localhost:8000/users/" + id, {
+        method: "DELETE",
+        
+      }).then((response) => {
+        if(!response.ok){
+          console.log('Error deleting row');
+        }
+        return response.json()
+      })
+      ;
+  
+      return promise;
+    }
+  
     
     return (
         <div className="container">
